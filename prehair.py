b8118b4f6b22a4755a92414e56a66046b4870e10
cfsanche <cam7sanchez@gmail.com>
2018-01-18 08:55:40 -0500

    fixed

--- prehair.py
+++ prehair.py
@@ -6,27 +6,34 @@ NOTES:
 import numpy as np
 import cv2 #step1
 #STEP 1: Homomorphic Filter 
-#import image
-img = cv2.imread('C:\\Users\\Camila\\Pictures\\Capstone\\lesion_test.jpg',-1) # 0 = grayscale
-imgg = cv2.imread('C:\\Users\\Camila\\Pictures\\Capstone\\lesion_test.jpg',0) # 0 = grayscale
+#import image in colour 
+#transform image into CIE L*a*b* colour space
+#appy homomorphic filter to the L* (luminance) plane
+# REFERENCE: https://blogs.mathworks.com/steve/2013/06/25/homomorphic-filtering-part-1/
+# https://www.youtube.com/watch?v=Q2_PD6MGOoM
+# http://biomedpharmajournal.org/vol7no2/image-sharpening-by-gaussian-and-butterworth-high-pass-filter/
+# http://www.ipcsit.com/vol45/015-ICIKM2012-M0029.pdf
+
+img = cv2.imread('C:\\Users\\Camila\\Pictures\\Capstone\\lesion_test.jpg',1) # 0 = grayscale
+
 
 img1 = np.float32(img)
 img1 = img1/255
 rows,cols,dim=img1.shape
 
-rh, rl, cutoff = 2.5,0.5,32
+rh, rl, cutoff = 2,0.5,10
 
 imgYCrCb = cv2.cvtColor(img1, cv2.COLOR_BGR2YCrCb)
 y,cr,cb = cv2.split(imgYCrCb)
 
 y_log = np.log(y+0.01)
 
-y_fft = np.fft.fft2(y_log)
+y_fft = np.fft.fft2(y_log) #gaussian filter is centered so centre this
 
 y_fft_shift = np.fft.fftshift(y_fft)
 
 
-DX = cols/cutoff
+DX = 0.25 # cutoff/255
 G = np.ones((rows,cols))
 for i in range(rows):
     for j in range(cols):
@@ -34,11 +41,16 @@ for i in range(rows):
 
 result_filter = G * y_fft_shift
 
-result_interm = np.real(np.fft.ifft2(np.fft.ifftshift(result_filter)))
+result_interm = np.float32(np.exp(np.real(np.fft.ifft2(np.fft.ifftshift(result_filter)))))
 
-result = np.exp(result_interm)
-cv2.imshow('image1',imgg)
+result = cv2.merge((result_interm,cr,cb))
+result = cv2.cvtColor(result,cv2.COLOR_YCrCb2BGR)
+img = cv2.resize(img,(752,565))
+result = cv2.resize(result,(752,565))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+result = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+cv2.imshow('image1',img)
 cv2.imshow('image2',result)
-cv2.waitKey(0)
-cv2.waitKey() #necessary? -yes
-
+cv2.waitKey(0) #necessary? -yes
+result = np.uint8(result*255)
+cv2.imwrite('cutoff_10.png',result)
